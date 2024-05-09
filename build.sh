8bf4348a319a1779fc4329e61ee740ae9e28b42e
Nathan Bird <nathan.bird@familysearch.org>
2024-05-09 14:33:39 -0600

    change build script

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@
 # rm -rf dist
 
 # Run pyinstaller to package the punch.py script
+<<<<<<< HEAD
 /Library/Frameworks/Python.framework/Versions/Current/bin/pyinstaller punch.py --onedir --noconfirm
+=======
+pyinstaller punch.py --onedir --noconfirm
+>>>>>>> 4f40ad7 (Create build script for shell)
 
 # Remove the generated punch.spec file
 rm punch.spec
